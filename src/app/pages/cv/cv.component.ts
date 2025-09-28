09ffdb5d596dccf9447affbee778b1e1274c6ae3
paq1 <p.paquin1605@gmail.com>
2025-09-28 21:03:05 +0200

    wip: structure de la page

--- src/app/pages/cv/cv.component.ts
+++ src/app/pages/cv/cv.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-cv',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './cv.component.html',
   styleUrl: './cv.component.scss'
 })
