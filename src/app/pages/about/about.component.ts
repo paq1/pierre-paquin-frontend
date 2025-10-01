b91c2ef79c321d72173ba3515452865eab732b86
paq1 <p.paquin1605@gmail.com>
2025-10-01 21:55:20 +0200

    wip: ajout du about

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,11 +1,30 @@
 import {Component} from '@angular/core';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+  selected: string = "dev";
 
+  get isSelectedDev(): boolean { return this.selected === "dev"; }
+  get isSelectedEtude(): boolean { return this.selected === "etudes"; }
+  get isSelectedDoggoverse(): boolean { return this.selected === "doggoverse"; }
+
+  onSelectDev(): void {
+    this.selected = "dev";
+  }
+
+  onSelectEtudes(): void {
+    this.selected = "etudes";
+  }
+
+  onSelectDoggoverse(): void {
+    this.selected = "doggoverse";
+  }
 }
