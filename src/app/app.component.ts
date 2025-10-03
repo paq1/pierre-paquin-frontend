ac2ca191b90821afb343e3f36fbf94d46326efc8
paq1 <p.paquin1605@gmail.com>
2025-10-03 17:02:10 +0200

    chore: clean

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {MenuComponent} from './pages/shared/menu/menu.component';
-import {ProceduralBgComponent} from './pages/shared/procedural-bg/procedural-bg.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuComponent, ProceduralBgComponent],
+  imports: [RouterOutlet, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
